eeb8f337d891dc94c242435406b73c26c5eb2877
b2trumpet <btrumpet@live.com>
2015-03-05 12:46:07 -0500

    Cleaning Up
    Added favicon. Renamed folders/file to remove capitals and spaces. Made
    adjustments to make the site look better on mobile devices.

--- index.php
+++ index.php
@@ -4,10 +4,11 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="favicon.ico">
+    <meta name="theme-color" content="#5f9b5c">
+    <link rel="icon" href="images/favicon.ico">
 
     <title>GEM Global Network</title>
 
@@ -109,7 +110,7 @@ Shadowbox.init();
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Community<span class="caret"></span></a>
                         <ul class="dropdown-menu" role="menu">
-                            <li><a href="#">GEM Members</a>
+                            <li><a href= "donate.html">GEM Members</a>
                             </li>
                             <li><a href="#">Community Agreements</a>
                             </li>
