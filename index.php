f624bc373969f89411dfcf2bf75425437e6bc852
b2trumpet <btrumpet@live.com>
2015-03-05 13:23:24 -0500

    Header
    Created "includes/header.php" to establish a universal header that will
    be used on all pages.

--- index.php
+++ index.php
@@ -1,36 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <meta name="theme-color" content="#5f9b5c">
-    <link rel="icon" href="images/favicon.ico">
-
-    <title>GEM Global Network</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="css/styles.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="shadowbox.css">
-<script type="text/javascript" src="shadowbox.js"></script>
-<script type="text/javascript">
-Shadowbox.init();
-</script>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-</head>
-
-<body>
+<?php include "include/header.php" ?>
+    
+    
 	<div class="trans-modal">
 		<div style="height:15%">
 		
@@ -46,99 +16,9 @@ Shadowbox.init();
 			<button id="submit" type="submit" onclick="RegisterEmail()">Submit</button>
 		</div>
 	</div>
-    <div class="utility-nav">
-        <div class="container">
-            <ul class="pull-right">
-                <li><a href="#"><i class="fa fa-lock"></i> user profile</a>
-                </li>
-                <li><a href="#"><i class="fa fa-share-square"></i> share</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-        <div class="container">
-        </div>
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">GEM Global Network</a>
-            </div>
-            <div id="navbar" class="navbar-collapse collapse">
-                <ul class="nav navbar-nav navbar-right">
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">About<span class="caret"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="About/About-us.html">About GEM</a>
-                            </li>
-                            <li><a href="#">Why Join GEM</a>
-                            </li>
-                            <li><a href="#">GEMs Headquarters</a>
-                            </li>
-                            <li><a href="#">Our Team</a>
-                            </li>
-                            <li><a href="#">Partners</a>
-                            </li>
-                            <li><a href="#">Sponsors</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Get Involved<span class="caret"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-                       		<li><a href="#">Join the GEM Network</a>
-                            </li>
-                            <li><a href="#">Create a Project</a>
-                            </li>
-                            <li><a href="#">Join a Project</a>
-                            </li>
-                            <li><a href="#">Create a Youth Circle</a>
-                            </li>
-                            <li><a href="#">Join a Youth Circle</a>
-                            </li>
-                            <li><a href="#">Invite GEM to Your School</a>
-                            </li>
-                            <li><a href="#">Volunteer</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Community<span class="caret"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href= "donate.html">GEM Members</a>
-                            </li>
-                            <li><a href="#">Community Agreements</a>
-                            </li>
-                            <li><a href="#">Community Map</a>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="#">The Mandala</a>
-                    </li>
-                       <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Support<span class="caret"></span></a>
-                         <ul class="dropdown-menu" role="menu">
-                            <li><a href="#">Donate to GEM</a>
-                            </li>
-                            <li><a href="#">Volunteer</a>
-                            </li>
-                            <li><a href="#">Give One Day</a>
-                        </ul>
-                    </li>
-
-                    <li><a href="#">Contact</a>
-                    </li>
-
-                </ul>
-            </div>
-            <!--/.navbar-collapse -->
-        </div>
-    </nav>
+    
+    
+   
     <div class="container">
         <div class="row">
             <div class="col-md-5 col-md-offset-7 col-lg-5 col-md-offset-7">
